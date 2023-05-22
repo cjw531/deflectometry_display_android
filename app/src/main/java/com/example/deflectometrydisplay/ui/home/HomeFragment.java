97caaa014ee93b6b41141480fb0c6142630dd6c8
Jiwon Choi <cjwon531@gmail.com>
2023-05-22 13:01:17 +0900

    device pixel dimension information

--- app/src/main/java/com/example/deflectometrydisplay/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/deflectometrydisplay/ui/home/HomeFragment.java
@@ -1,13 +1,18 @@
 package com.example.deflectometrydisplay.ui.home;
 
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.deflectometrydisplay.databinding.FragmentHomeBinding;
@@ -18,14 +23,44 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView text_notch = binding.textNotch;
+        homeViewModel.hasNotch(requireContext()).observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String has_notch) {
+                String notch_information = "Notch Exists (Y/N): " + has_notch;
+                text_notch.setText(notch_information);
+            }
+        });
+
+        final TextView text_notch_dimension = binding.textNotchSize;
+        homeViewModel.getNotchHeight(requireContext()).observe(getViewLifecycleOwner(), new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer height) {
+//                int screenWidth = dimensions[0];
+//                int screenHeight = dimensions[1];
+
+                String notch_dimensions = "Notch Height: " + height;
+                text_notch_dimension.setText(notch_dimensions);
+            }
+        });
+
+        final TextView text_dimension = binding.textDimension;
+        homeViewModel.get_pixel_dimensions(requireContext()).observe(getViewLifecycleOwner(), new Observer<int[]>() {
+            @Override
+            public void onChanged(int[] dimensions) {
+                int screenWidth = dimensions[0];
+                int screenHeight = dimensions[1];
+
+                String fullScreenDimensions = "Util. Screen: " + screenWidth + " x " + screenHeight;
+                text_dimension.setText(fullScreenDimensions);
+            }
+        });
+
         return root;
     }
 
--- app/src/main/java/com/example/deflectometrydisplay/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/deflectometrydisplay/ui/home/HomeViewModel.java
@@ -1,8 +1,19 @@
 package com.example.deflectometrydisplay.ui.home;
 
+import android.content.Context;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.DisplayCutout;
+import android.view.View;
+import android.view.WindowManager;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import java.util.List;
 
 public class HomeViewModel extends ViewModel {
 
@@ -12,6 +23,65 @@ public class HomeViewModel extends ViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is home fragment");
     }
+
+    public LiveData<int[]> get_pixel_dimensions(Context context) {
+        MutableLiveData<int[]> pixelDimensionsLiveData = new MutableLiveData<>();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+
+        int width = displayMetrics.widthPixels;
+        int height = displayMetrics.heightPixels;
+
+        int[] dimensions = {width, height};
+        pixelDimensionsLiveData.setValue(dimensions);
+
+        return pixelDimensionsLiveData;
+    }
+
+    public LiveData<String> hasNotch(Context context) {
+        MutableLiveData<String> has_notch = new MutableLiveData<>();
+        has_notch.setValue("N");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            if (windowManager != null) {
+                DisplayCutout displayCutout = windowManager.getDefaultDisplay().getCutout();
+                if (displayCutout != null && displayCutout.getBoundingRects() != null && displayCutout.getBoundingRects().size() > 0) {
+                    has_notch.setValue("Y");
+                }
+            }
+        } else {
+            // TODO: currently null implementation
+            // Check for other methods to detect notches on older Android versions
+        }
+
+        return has_notch;
+    }
+
+    public LiveData<Integer> getNotchHeight(Context context) {
+        MutableLiveData<Integer> notchHeightLiveData = new MutableLiveData<>();
+        notchHeightLiveData.setValue(0);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            if (windowManager != null) {
+                Display display = windowManager.getDefaultDisplay();
+                DisplayCutout displayCutout = display.getCutout();
+                if (displayCutout != null && displayCutout.getBoundingRects() != null && displayCutout.getBoundingRects().size() > 0) {
+                    List<Rect> boundingRects = displayCutout.getBoundingRects();
+                    // Assuming a single notch, retrieve the dimensions of the first bounding rectangle
+                    Rect notchRect = boundingRects.get(0);
+                    int notchHeight = notchRect.height();
+                    notchHeightLiveData.setValue(notchHeight);
+                }
+            }
+        }
+
+        return notchHeightLiveData;
+    }
+
 
     public LiveData<String> getText() {
         return mText;
